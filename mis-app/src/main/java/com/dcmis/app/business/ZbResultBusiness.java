4a15a76b90202d8b5c7c9ef2d20ac28ba96dee14
huangfuliang <1416011240@qq.com>
2017-07-31 16:12:25 +0800

    20170731

---
+++ mis-app/src/main/java/com/dcmis/app/business/ZbResultBusiness.java
@@ -0,0 +1,37 @@
+package com.dcmis.app.business;
+
+import com.dcmis.app.dao.DaoSupport;
+import com.dcmis.app.util.PageData;
+import com.github.pagehelper.PageHelper;
+
+import java.util.List;
+
+/**
+ * Created by 黄福亮 on 2017/7/31.
+ */
+public class ZbResultBusiness {
+
+    /**
+     * 查询报表页面的指标信息
+     * @param pd
+     * @param databaseId
+     * @param daoSupport
+     * @return
+     * @throws Exception
+     */
+    public PageData getRptResult(PageData pd, String databaseId, DaoSupport daoSupport) throws Exception {
+        PageData reData = new PageData();
+        int curPage = (Integer.parseInt(pd.getString("iDisplayStart")) / Integer.parseInt(pd.getString("iDisplayLength")) + 1);
+        int pageSize = Integer.parseInt(pd.getString("iDisplayLength"));
+        String sEcho = pd.getString("sEcho");
+        PageHelper.startPage(curPage, pageSize);
+        List<PageData> pds = (List<PageData>)daoSupport.findForList("zbDataMapper.getRptZbMsg",pd);
+        PageData pdNum = (PageData)daoSupport.findForObject("zbDataMapper.getRptZbMsgTotalCount", pd);
+        reData.put("aaData", pds);
+        reData.put("sEcho", sEcho);
+        reData.put("iTotalRecords", pdNum.get("COUNT").toString());
+        reData.put("iTotalDisplayRecords", pdNum.get("COUNT").toString());
+        return reData;
+    }
+
+}
