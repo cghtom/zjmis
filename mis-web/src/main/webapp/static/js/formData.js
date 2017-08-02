77859ec23825e29d02ef59e367b4078d80fbfb4d
newbieAnd <18609645463@163.com>
2017-08-02 14:07:06 +0800

    修改参数传递错误

--- mis-web/src/main/webapp/static/js/formData.js
+++ mis-web/src/main/webapp/static/js/formData.js
@@ -2,8 +2,8 @@ seajs.use(['laytpl'],function(laytpl){
     $('#contentBody').on('click','#rpt_detail',function() {
         var $this = $(this);
         var rptId = $this.attr('data1');
-        var opTime = $this.attr('data2');
-        var url = 'zbData.html?rptId='+rptId + "&opTime="+opTime;
+        var opTime = $this.attr('data2').replace("-","");
+        var url = 'zbData.html?RPT_ID='+rptId + "&OP_TIME="+opTime;
         $('#zbModel').modal();
         $('#zbIfr').attr('src',url);
     });
