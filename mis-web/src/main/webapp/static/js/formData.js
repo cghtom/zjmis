68c93dfae734697063c187fc914c5eb794cf24c4
newbieAnd <18609645463@163.com>
2017-08-03 09:37:47 +0800

    新增归档功能JS

--- mis-web/src/main/webapp/static/js/formData.js
+++ mis-web/src/main/webapp/static/js/formData.js
@@ -7,4 +7,25 @@ seajs.use(['laytpl'],function(laytpl){
         $('#zbModel').modal();
         $('#zbIfr').attr('src',url);
     });
+    
+    $("#file-button").on("click",function () {
+        var checkboxArr = $("#table_report").find("td").find("input:checked");
+        var rpt_id = "";
+        if (checkboxArr.length > 0) {
+            for(var i=0;i<checkboxArr.length;i++){
+                if(i==checkboxArr.length-1){
+                    rpt_id += $(checkboxArr[i]).val()+"";
+                }
+                else{
+                    rpt_id += $(checkboxArr[i]).val()+",";
+                }
+            }
+            var param = ("&serviceName=" + fileServiceName+"&RPT_ID="+rpt_id);
+            $.get(httpUrl + "&" + param, function(data){
+                alert("success!")
+            });
+        }else{
+            alert("未选择要归档的报表！")
+        }
+    })
 })
