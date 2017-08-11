f859c7aaf5ed6315c463b42069f67dc40c91ee97
newbieAnd <18609645463@163.com>
2017-08-11 15:01:15 +0800

    新增下载功能

--- mis-app/src/main/java/com/dcmis/app/business/RptResultBusiness.java
+++ mis-app/src/main/java/com/dcmis/app/business/RptResultBusiness.java
@@ -1,15 +1,20 @@
 package com.dcmis.app.business;
 
+import com.alibaba.fastjson.JSONArray;
 import com.dcmis.app.dao.DaoSupport;
+import com.dcmis.app.entity.RptEntity;
 import com.dcmis.app.util.PageData;
 import com.github.pagehelper.PageHelper;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by 黄福亮 on 2017/7/28.
  */
 public class RptResultBusiness {
+
     /**
      * 查询报表结果信息 MISGETRPTRESULT
      * @param pd
@@ -43,8 +48,39 @@ public class RptResultBusiness {
      */
     public void fileMisRptMsg (PageData pd, String databaseId, DaoSupport daoSupport) throws Exception{
         Object update = daoSupport.update("zbDataMapper.fileDataByTmp", pd);
-        System.out.println(update+"--------");
-        System.out.println("123");
+    }
+
+    public PageData getAllRptMsg(PageData pd, String databaseId, DaoSupport daoSupport) throws Exception{
+        PageData reData = new PageData();
+        List<PageData> pds = (List<PageData>)daoSupport.findForList("rptDataMapper.getRptResult",pd);
+        reData.put("rptMsg", pds);
+        return reData;
+    }
+
+    public PageData  exportExcel(PageData pd, String databaseId, DaoSupport daoSupport) throws  Exception{
+        List<PageData> pds = (List<PageData>)daoSupport.findForList("rptDataMapper.getRptResult",pd);
+        String rptID = "";
+        String rptName = "";
+        JSONArray ja = new JSONArray();
+        for(int i=0;i<pds.size();i++){
+            PageData rptMsg = pds.get(i);
+            rptID = rptMsg.getString("RPT_ID");
+            rptName = rptMsg.getString("RPT_NAME");
+            RptEntity entity = new RptEntity(rptID,rptName);
+            ja.add(entity);
+        }
+        Map<String,String> headMap = new LinkedHashMap<String,String>();
+        headMap.put("rptID","指标编码");
+        headMap.put("rptName","只表名称");
+        String title = "测试";
+       /* String curFile = constant.getLocPath()+"test.xlsx";
+        OutputStream outXlsx = new FileOutputStream(curFile);
+        System.out.println("正在导出xlsx....");
+        ExportExcelUtil.exportExcelX(title,headMap,ja,null,0,outXlsx);
+        outXlsx.close();
+        String remoteFile = constant.getPath()+"test.xlsx";
+        ftpUtil.put(curFile, remoteFile);*/
+        return null;
     }
 
 }
