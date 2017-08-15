07f0126444d2a1bf2ee13481977c99abfb02b153
newbieAnd <18609645463@163.com>
2017-08-15 14:14:27 +0800

    下载报表相关信息的实体类

--- mis-app/src/main/java/com/dcmis/app/entity/RptEntity.java
+++ mis-app/src/main/java/com/dcmis/app/entity/RptEntity.java
@@ -7,12 +7,27 @@ public class RptEntity {
 
     private String rptID;
 
-    private String rptName;
-
-    public RptEntity(String rptID, String rptName) {
+    public RptEntity(String rptID, String opTime, String zbID, String zbName, String cityID, String zbData1, String zbData2) {
         this.rptID = rptID;
-        this.rptName = rptName;
+        this.opTime = opTime;
+        this.zbID = zbID;
+        this.zbName = zbName;
+        this.cityID = cityID;
+        this.zbData1 = zbData1;
+        this.zbData2 = zbData2;
     }
+
+    private String opTime;
+
+    private String zbID;
+
+    private String zbName;
+
+    private String cityID;
+
+    private String zbData1;
+
+    private String zbData2;
 
     public String getRptID() {
         return rptID;
@@ -22,13 +37,54 @@ public class RptEntity {
         this.rptID = rptID;
     }
 
-    public String getRptName() {
-        return rptName;
+    public String getOpTime() {
+        return opTime;
     }
 
-    public void setRptName(String rptName) {
-        this.rptName = rptName;
+    public void setOpTime(String opTime) {
+        this.opTime = opTime;
     }
+
+    public String getZbID() {
+        return zbID;
+    }
+
+    public void setZbID(String zbID) {
+        this.zbID = zbID;
+    }
+
+    public String getZbName() {
+        return zbName;
+    }
+
+    public void setZbName(String zbName) {
+        this.zbName = zbName;
+    }
+
+    public String getCityID() {
+        return cityID;
+    }
+
+    public void setCityID(String cityID) {
+        this.cityID = cityID;
+    }
+
+    public String getZbData1() {
+        return zbData1;
+    }
+
+    public void setZbData1(String zbData1) {
+        this.zbData1 = zbData1;
+    }
+
+    public String getZbData2() {
+        return zbData2;
+    }
+
+    public void setZbData2(String zbData2) {
+        this.zbData2 = zbData2;
+    }
+
 
 
 
