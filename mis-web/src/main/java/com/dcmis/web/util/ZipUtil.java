0bb26a2410751245235043fd4bd5d7301efd4869
newbieAnd <18609645463@163.com>
2017-08-16 10:12:34 +0800

    将文件压缩成zip的工具类

--- mis-web/src/main/java/com/dcmis/web/util/ZipUtil.java
+++ mis-web/src/main/java/com/dcmis/web/util/ZipUtil.java
@@ -5,31 +5,11 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class ZipUtil {
-    public static void main(String[] args) {
-        StringBuffer sb=new StringBuffer();
-        try{
-            sb.append("D:/doc1/aa.xlsx,");
-            sb.append("D:/doc1/bb.xlsx,");
-            sb.append("D:/doc1/cc.xlsx,");
-            sb.append("D:/doc1/dd.xlsx,");
-            sb.append("D:/doc1/ee.xlsx,");
-//              list.add("D:/doc1/aa.xlsx");
-//              list.add("D:/doc1/bb.xlsx");
-//              list.add("D:/doc1/vv.xlsx");
-//              list.add("D:/doc1/dd.xlsx");
-//              list.add("D:/doc1/ee.xlsx");
-            //文件的列表 和 将要打成的zip文件的名称
-//              String[] files = (String[]) list.toArray();
-//              System.out.println(files);
-            writeZip(sb,"newZipFile");
-        } catch ( IOException e ) {
-            e.printStackTrace();
-        }
-    }
+
     public static void writeZip(StringBuffer sb,String zipname) throws IOException {
         String[] files = sb.toString().split(",");
         System.out.println(files);
-        OutputStream os = new BufferedOutputStream( new FileOutputStream(zipname ) );
+        OutputStream os = new BufferedOutputStream( new FileOutputStream( zipname ) );
         ZipOutputStream zos = new ZipOutputStream( os );
         byte[] buf = new byte[8192];
         int len;
