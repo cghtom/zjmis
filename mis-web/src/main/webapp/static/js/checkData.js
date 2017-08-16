082f33bdc6b2505fb4dea0362b4cf94b74f930e5
newbieAnd <18609645463@163.com>
2017-08-16 10:06:09 +0800

    优化页面效果，新增条件查询

--- mis-web/src/main/webapp/static/js/checkData.js
+++ mis-web/src/main/webapp/static/js/checkData.js
@@ -13,8 +13,21 @@ seajs.use(['laytpl',
                 var tableConfig = eval('(' + data.TABLE_CONFIG + ')');
                 var dataModel = eval('(' + data.MODEL + ')');
                 var dataTab = $('#table_report').dataTable(tableConfig);
+                $("#rpt_id").val(arr.RPT_ID);
+                $("#op_time").val(arr.OP_TIME);
 
-
+                $('#query-button').on('click',function() {
+                    var serviceName = "MISGETCHECKRESULT";
+                    var CHECK_RESULT = $("#check_result").val();
+                    var RPT_ID = $("#rpt_id").val();
+                    var OP_TIME = $("#op_time").val();
+                    var CITY_ID = $("#city_id").val();
+                    httpUrl = (httpUrlQuery+"&serviceName="+serviceName+"&CHECK_RESULT="+CHECK_RESULT+"&OP_TIME="+OP_TIME+"&CITY_ID="+CITY_ID+"&RPT_ID=" + RPT_ID )
+                    console.log(httpUrl);
+                    var oSettings = dataTab.fnSettings();
+                    oSettings.sAjaxSource = httpUrl;//重新设置url
+                    dataTab.fnDraw(false);
+                });
 
                 $('table th input:checkbox').on('click' , function(){
                     var that = this;
